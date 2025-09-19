71c3d27ea14afea6ed13d67c39b2dd42127248e4
Alessandro Rodrigues <alessandro.melo01@fatec.sp.gov.br>
2025-09-19 20:36:40 -0300

    fix deploy.yml
    Fix do arquivo deploy

---
+++ eslint.config.js
@@ -0,0 +1,48 @@
+// eslint.config.js
+// @ts-check
+import js from "@eslint/js";
+import jest from "eslint-plugin-jest"; // <-- 1. Importe o plugin do Jest
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  // arquivos/paths a ignorar
+  {
+    ignores: ["node_modules/**", "coverage/**", "dist/**"]
+  },
+
+  // regras recomendadas do ESLint
+  js.configs.recommended,
+
+  // opções gerais do projeto
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        // Node
+        process: "readonly",
+        console: "readonly",
+        __dirname: "readonly",
+        module: "readonly",
+        require: "readonly",
+        // Remova o 'jest: "true"' daqui, pois o plugin vai cuidar disso
+      }
+    },
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "no-undef": "error"
+    }
+  },
+
+  // ajustes específicos para testes
+  {
+    // 2. Aplique a configuração recomendada do Jest aqui
+    ...jest.configs['flat/recommended'],
+    files: ["tests/**/*.test.js"],
+    rules: {
+      // 3. Mantenha suas regras personalizadas e as do Jest
+      ...jest.configs['flat/recommended'].rules,
+      "no-console": "off"
+    }
+  }
+];
